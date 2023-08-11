017f403b4a0f43dcc9f6eaa4f68807638827a305
bobzheng <shenyanzheng00@gmail.com>
2023-08-11 15:08:39 +0800

    home page set up

--- javascript/home_navbar.js
+++ javascript/home_navbar.js
@@ -4,19 +4,23 @@ const ul = document.querySelector("ul");
 const lineDash = document.querySelector(".line-dash");
 
 
-var dashOrigin = -35; //pixels
-var selectedLi = -35; //pixels
-var speed = 500; //move this many pixels in one second.
+var dashOrigin = -35;
+var selectedLi = -35; //init position
+var speed = 500;
 var distance = 0;
 var time = 0;
 
-TweenLite.to(lbs[0], 0.6, {
+TweenLite.to(lineDash, time, {  //origin position of the upper bracket
+	strokeDashoffset: -35,
+	ease: Bounce.easeOut});
+
+TweenLite.to(lbs[0], 0.6, {  //origin position of the lower bracket
 					y: -43,
 					ease: Bounce.easeOut,
 					delay: 1
 				});
 
-lis[0].classList.add("active");
+lis[0].classList.add("active");  //origin height light of the text
 
 function pushDownLb() {
 	for(let k = 0; k < lbs.length; ++k)
@@ -35,7 +39,7 @@ ul.addEventListener(
 			time = distance / speed;
 			dashOrigin = selectedLi;
 			if (time) {
-				// overlaping tweens would give a zero time
+
 				TweenLite.to(lineDash, time, {
 					strokeDashoffset: selectedLi,
 					ease: Bounce.easeOut
@@ -46,21 +50,21 @@ ul.addEventListener(
 	false
 );
 
-for (let i = 0; i < 4; ++i) {
+for (let i = 0; i < 5; ++i) {
 	lis[i].addEventListener("mouseover", function() {
-		distance = Math.abs(-250 * i - 35 - dashOrigin);
+		distance = Math.abs(-200 * i - 35 - dashOrigin);
 		time = distance / speed;
-		dashOrigin = -250 * i - 35;
+		dashOrigin = -200 * i - 35;
 		if (time) {
 			TweenLite.to(lineDash, time, {
-				strokeDashoffset: -250 * i - 35,
+				strokeDashoffset: -200 * i - 35,
 				ease: Bounce.easeOut
 			});
 		} //if
 	});
 
 	lis[i].addEventListener("click", function() {
-		selectedLi = -250 * i - 35;
+		selectedLi = -200 * i - 35;
 		pushDownLb();
 		let current = document.getElementsByClassName("active");
 		current[0].classList.remove("active");
