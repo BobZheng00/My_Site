c12b8e0559a962a739671d6754c9ccbf93f55366
bobzheng <shenyanzheng00@gmail.com>
2023-08-12 13:56:23 +0800

    home page set up

--- javascript/home.js
+++ javascript/home.js
@@ -75,3 +75,13 @@ for (let i = 0; i < 5; ++i) {
 				});
 	});
 }
+
+
+document.getElementById("body").onscroll = function myFunction() {
+    var scrolltotop = document.scrollingElement.scrollTop;
+    var target = document.getElementById("content");
+    var xvalue = "center";
+    var factor = 0.5;
+    var yvalue = scrolltotop * factor;
+    target.style.backgroundPosition = xvalue + " " + yvalue + "px";
+  }
